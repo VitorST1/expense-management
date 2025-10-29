638e0a5101ac4947bf0429b2303f22be067a9e0d
Vitor <vitorturqueti@hotmail.com>
2025-10-28 21:40:46 -0300

    feat(db): add categories and expenses schemas

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,13 +2,23 @@ import { defineSchema, defineTable } from 'convex/server'
 import { v } from 'convex/values'
 
 export default defineSchema({
-  products: defineTable({
-    title: v.string(),
-    imageId: v.string(),
-    price: v.number(),
-  }),
   todos: defineTable({
     text: v.string(),
     completed: v.boolean(),
   }),
+  categories: defineTable({
+    name: v.string(),
+    userId: v.string(),
+  })
+    .index('by_user', ['userId']),
+  expenses: defineTable({
+    category: v.id('categories'),
+    description: v.optional(v.string()),
+    amount: v.number(),
+    date: v.number(),
+    userId: v.string(),
+  })
+    .index('by_user', ['userId'])
+    .index('by_category', ['category'])
+    .index('by_date', ['date']),
 })
