469d55c9ed8bda872f0e9b18c13821a055eede8a
Vitor <vitorturqueti@hotmail.com>
2025-11-09 15:41:20 -0300

    feat(categories): add delete functionality to category table

--- src/components/categories/CategoryTable.tsx
+++ src/components/categories/CategoryTable.tsx
@@ -1,12 +1,26 @@
-import { convexQuery } from "@convex-dev/react-query"
-import { useSuspenseQuery } from "@tanstack/react-query"
+import { convexQuery, useConvexMutation } from "@convex-dev/react-query"
+import { useMutation, useSuspenseQuery } from "@tanstack/react-query"
 import { api } from "convex/_generated/api"
 import { DataTable } from "../ui/DataTable"
 import { columns } from "./columns"
 import { m } from "@/paraglide/messages"
+import { Id } from "convex/_generated/dataModel"
+import { toast } from "sonner"
 
 export default function CategoryTable() {
     const { data, error, isLoading,  } = useSuspenseQuery(convexQuery(api.categories.get, {}))
+    const { mutateAsync: deleteCategory } = useMutation({
+        mutationFn: useConvexMutation(api.categories.remove),
+    })
+
+    const handleDelete = async (id: Id<"categories">) => {
+        try {
+            await deleteCategory({id})
+        } catch (error) {
+            console.error(error);
+            toast.error(m.error_occurred());
+        }
+    }
 
     if (error) {
         console.error(error)
@@ -15,5 +29,9 @@ export default function CategoryTable() {
         </div>
     }
 
-    return <DataTable columns={columns} data={data} isLoading={isLoading} />
+    return <DataTable
+        columns={columns({ onDelete: handleDelete })}
+        data={data}
+        isLoading={isLoading} 
+    />
 }
--- src/components/categories/columns.tsx
+++ src/components/categories/columns.tsx
@@ -1,11 +1,15 @@
 import { ColumnDef } from "@tanstack/react-table"
-import { Doc } from "convex/_generated/dataModel"
+import { Doc, Id } from "convex/_generated/dataModel"
 import { m } from "@/paraglide/messages"
 import { Button } from "../ui/button"
 import { PencilIcon, Trash2Icon } from "lucide-react"
 import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip"
 
-export const columns: ColumnDef<Doc<"categories">>[] = [
+export const columns = ({
+    onDelete,
+}: {
+    onDelete: (id: Id<"categories">) => void
+}): ColumnDef<Doc<"categories">>[] => [
     {
         accessorKey: "name",
         header: m.name(),
@@ -14,27 +18,34 @@ export const columns: ColumnDef<Doc<"categories">>[] = [
         accessorKey: "actions",
         header: "",
         meta: { className: "w-0" },
-        cell: () => <div className="flex gap-2">
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <Button className="bg-blue-500 text-zinc-100 hover:bg-blue-700" size="icon-sm">
-                        <PencilIcon />
-                    </Button>
-                </TooltipTrigger>
-                <TooltipContent className="bg-blue-700 text-zinc-100" arrowClassName="bg-blue-700 fill-blue-700" side="bottom">
-                    {m.edit()}
-                </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-                <TooltipTrigger asChild>
-                    <Button className="bg-red-500 text-zinc-100 hover:bg-red-700" size="icon-sm">
-                        <Trash2Icon />
-                    </Button>
-                </TooltipTrigger>
-                <TooltipContent className="bg-red-700 text-zinc-100" arrowClassName="bg-red-700 fill-red-700" side="bottom">
-                    {m.delete()}
-                </TooltipContent>
-            </Tooltip>
-        </div>,
+        cell: ({ row }) => {
+            const category = row.original
+            return <div className="flex gap-2">
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button className="bg-blue-500 text-zinc-100 hover:bg-blue-700" size="icon-sm">
+                            <PencilIcon />
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent className="bg-blue-700 text-zinc-100" arrowClassName="bg-blue-700 fill-blue-700" side="bottom">
+                        {m.edit()}
+                    </TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Button
+                            className="bg-red-500 text-zinc-100 hover:bg-red-700"
+                            size="icon-sm"
+                            onClick={() => onDelete(category._id)}
+                        >
+                            <Trash2Icon />
+                        </Button>
+                    </TooltipTrigger>
+                    <TooltipContent className="bg-red-700 text-zinc-100" arrowClassName="bg-red-700 fill-red-700" side="bottom">
+                        {m.delete()}
+                    </TooltipContent>
+                </Tooltip>
+            </div>
+        },
     }
 ]
